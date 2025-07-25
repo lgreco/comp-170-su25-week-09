d82a6218078f8b90efbc791c850229395c2d6f5a
Leo Irakliotis <lgreco@gmail.com>
2025-07-24 21:51:10 -0500

    added comments

--- week09.py
+++ week09.py
@@ -27,18 +27,24 @@ def fibonacci(n):
 def sum_of_digits_iterative(n):
     sum = 0
     while n > 1:
-        # Obtain the last digit to add to sum
+        # Obtain the last digit to add to sum. The last digit is always the remainder of
+        # the integer division by base of the number system in use (here: 10).
         sum += n % 10 
-        # Remove the last digit
+        # Remove the last digit. This can be done by integer division of the number with
+        # its number base (here: 10). For exampe 24 // 10 = 2 (and thus 4 is gone!)
         n //= 10
     # Done
     return sum + n
 
 def count_occurrences_iterative(data_list, target):
     count = 0
+    # Iterate over the input list
     for i in range(len(data_list)):
+        # Check if current list item matches target value
         if data_list[i] == target:
+            # If it does, increment the counter
             count += 1
+    # Done
     return count
 
 
